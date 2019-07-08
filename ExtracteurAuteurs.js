a587c0833b67ff83478f47be85dae89a59996d12
PhilippeGambette <philippe.gambette@laposte.net>
2019-07-08 07:52:37 +0200

    v1.3 debug Extracteur Auteurs
    - correction limitation à 100 auteurs
    - ajout d'un tableau dédoublonné par nom complet

--- ExtracteurAuteurs.js
+++ ExtracteurAuteurs.js
@@ -1,5 +1,5 @@
 /*
-    Extracteur Unpaywall, v1.2, 2019-06-13
+    Extracteur Unpaywall, v1.3, 2019-07-02
     Évaluer la proportion des publications en libre accès parmi une liste de DOI
     Copyright (C) 2018-2019 - Romain Boistel, Frédérique Bordignon, Philippe Gambette
 
@@ -20,7 +20,7 @@
 "use strict";
 $(document).ready(function(){
 // author list
-var author = [];
+var author = {};
 
 // Index of the latest author we tried to test
 var lastSentQuery = -1;
@@ -34,66 +34,109 @@ var info = new Object();
 // Timer to regularly launch queries
 var timer = "";
 
+// Get the structure id
+var structure = $("#structure").val()
+
+// Number of found publications
+var foundPublications = 0
+
 // Result
 var result = [];
-var oaByYear = new Object();
-var publicationsByYear = new Object();
-var graphDisplayed = false;
+var resultByName = {};
 
 $("#send").on("click",function(){
-   // Reinitialize lastSentQuery, lastReceivedQuery and info
+   // Reinitialize lastSentQuery, lastReceivedQuery and structure
    lastSentQuery = -1;
    lastReceivedQuery = -1;
+   structure = $("#structure").val()
    
    // Reinitialize the table
    result = [];
+   resultByName = {};
    
    // Extract the required field list
-   result[0] = $("#champsAuteurs").val().split(",");
+   result[0] = ("docid,"+$("#champsAuteurs").val()).split(",");
+   
+   // Fill in the title row of the result tables in the webpage
    $("#results").html('<tr id="titleRow"></tr>');
+   $("#results2").html('<tr id="titleRow2"></tr>');
    result[0].forEach(function(col){
       $("#titleRow").append("<th>"+col+"</th>");
+      $("#titleRow2").append("<th>"+col+"</th>");
    });
    
    // Extract the list of all authors of the structure
-   var url = "https://api.archives-ouvertes.fr/search/?q=*:*&rows=0&facet=true&facet.field=structHasAuthId_fs&facet.prefix="+$("#structure").val()+"_&wt=json"
-   $.get(url).done(authorList)
-   console.log(url)
+   //var url = "https://api.archives-ouvertes.fr/search/?q=*:*&rows=0&facet=true&facet.field=structHasAuthId_fs&facet.prefix="+$("#structure").val()+"_&wt=json"
+   var url = "http://api.archives-ouvertes.fr/search/?q=structId_i:"+structure+"&wt=json&fl=structHasAuthId_fs&rows=10000";
+   if($("#annees").val() != ""){
+      url = "http://api.archives-ouvertes.fr/search/?q=structId_i:"+structure+"&fq=producedDateY_i:"+$("#annees").val()+"&wt=json&fl=structHasAuthId_fs&rows=10000";
+   }
+   $.get(url).done(authorList);
+   console.log(url);
 })
 
 
 function authorList(data){
-   data.facet_counts.facet_fields.structHasAuthId_fs.forEach(function(item){
-      if(typeof(item)=="string"){
-         var authorInfo = item.split("_");
-         console.log(authorInfo);
-         if(authorInfo[4] != undefined){
-            author.push(authorInfo[4])
+   // Explore all publications:
+   for(var i in data.response.docs){
+      foundPublications += 1;
+      // Check all authors of the publication:
+      var authors = data.response.docs[i].structHasAuthId_fs;
+      for (var a in authors){
+         // Get the structure id and the author id with this regular expression:
+         var ids = /^([^_]*)_.*JoinSep_([^_]*)_FacetSep/.exec(authors[a]);
+         if(ids != null){
+            if(ids[1] == structure){
+               // If the author belongs to the structure, save it!
+               author[ids[2]] = 1;
+            }
          }
-      }      
-   })
-   console.log(author);
-   //Test a new author every second, in case the previous test was finished
-   timer = setInterval(sendQuery,3000);
-
+      }
+   }
+   
+   if(data.response.numFound>foundPublications){
+      var url = "http://api.archives-ouvertes.fr/search/?q=structId_i:"+structure+"&wt=json&fl=structHasAuthId_fs&rows=10000&start="+foundPublications;
+      if($("#annees").val() != ""){
+         url = "http://api.archives-ouvertes.fr/search/?q=structId_i:"+structure+"&fq=producedDateY_i:"+$("#annees").val()+"&wt=json&fl=structHasAuthId_fs&rows=10000&start="+foundPublications;
+      }
+      console.log(url);
+      $.get(url).done(authorList);
+   } else {
+      timer = setInterval(sendQuery,3000);
+   }
 }
 
 function sendQuery(){
    // Test the next author if the previous one was already tested
    if(lastSentQuery==lastReceivedQuery){      
-      // Reinitialize the object to store information about the next publication
+      // Reinitialize the object to store information about the next author
       info = new Object();
 
       // Send a new query
       lastSentQuery += 1;
       
-      if(lastSentQuery == author.length){
+      if(lastSentQuery == Object.keys(author).length){
          // All author have been sent, stop sending queries
          clearInterval(timer);
          $("#progress").html("100");
+         
+         // Fill in the table without duplicate names:
+         var allFullNames = Object.keys(resultByName);
+         // Loop through all authors (without duplicate names)
+         allFullNames.forEach(function(name){
+            // Loop through all information of this author
+            var row = "";
+            resultByName[name].forEach(function(element){
+               row += "<td>"+element+"</td>"
+            })
+            // Add a new row to the table without duplicate names
+            $("#results2").find("tbody").eq(0).append("\n<tr id=\"" + resultByName[name][0] + "\">" + row + "</tr>");
+         })         
+         
+         
       } else {
-         // Send the next author to Unpaywall
-         $.get("https://api.archives-ouvertes.fr/ref/author/?wt=json&q=docid:%22" + author[lastSentQuery] + "%22&fl=" + $("#champsAuteurs").val())
+         // Send the next author to HAL
+         $.get("https://api.archives-ouvertes.fr/ref/author/?wt=json&q=docid:%22" + Object.keys(author)[lastSentQuery] + "%22&fl=docid," + $("#champsAuteurs").val())
           .always(receiveHal);
       }
    }
@@ -101,82 +144,78 @@ function sendQuery(){
 
 // Save data received from HAL
 function receiveHal(data){
-   // Add a new row to the table
-   var row = "";
-   var resultRow = [];
-   result[0].forEach(function(key){
-      row += "<td>"+data.response.docs[0][key]+"</td>"
-      resultRow.push(data.response.docs[0][key])
-   })
-   $("#results").find("tbody").append("\n<tr id=\"" + author[lastSentQuery] + "\">" + row + "</tr>");
-   result[lastSentQuery+1] = resultRow;
-
-   /*
-   if(data.response.docs.length>0){
-      // HAL has found the DOI
-      info.halNb = data.response.numFound;
-      info.halId = '<a href="'+data.response.docs[0].uri_s+'">'+data.response.docs[0].halId_s+"</a>";
-      info.linkExtId = data.response.docs[0].linkExtId_s;
-      info.linkExtUrl = data.response.docs[0].linkExtUrl_s;
-      if(data.response.docs[0].linkExtUrl_s!=undefined){
-         info.linkExtUrl = "<a href=\""+data.response.docs[0].linkExtUrl_s+"\">"+data.response.docs[0].linkExtUrl_s+"</a>";
+   if(data.response.docs[0] != undefined){
+      // Add a new row to the table
+      var row = "";
+      var resultRow = [];
+      result[0].forEach(function(key){
+         if(key=="valid_s"){
+            if(data.response.docs[0][key] == "VALID"){
+               data.response.docs[0][key]="forme auteur principale d'un IdHAL";
+            }
+            if(data.response.docs[0][key] == "OLD"){
+               data.response.docs[0][key]="forme auteur alternative d'un IdHAL";
+            }
+            if(data.response.docs[0][key] == "INCOMING"){
+               data.response.docs[0][key]="forme auteur sans IdHAL associé";
+            }
+         }
+         row += "<td>"+data.response.docs[0][key]+"</td>"
+         resultRow.push(data.response.docs[0][key])
+      })
+      
+      // Add a new line to the result table in the webpage:
+      $("#results").find("tbody").eq(0).append("\n<tr id=\"" + Object.keys(author)[lastSentQuery] + "\">" + row + "</tr>");
+      
+      // Decide if the new author id must be added to the table without duplicate or not.
+      if(resultByName[data.response.docs[0]["fullName_s"]] != undefined){
+         if(resultByName[data.response.docs[0]["fullName_s"]]["valid_s"]=="forme auteur sans IdHAL associé"){
+            if(data.response.docs[0]["valid_s"] != "forme auteur sans IdHAL associé"){
+               resultByName[data.response.docs[0]["fullName_s"]] = resultRow;
+            }
+         }
+         if(resultByName[data.response.docs[0]["fullName_s"]]["valid_s"]=="forme auteur alternative d'un IdHAL"){
+            if(data.response.docs[0]["valid_s"] == "forme auteur principale d'un IdHAL"){
+               resultByName[data.response.docs[0]["fullName_s"]] = resultRow;
+            }
+         }
+      } else {
+         resultByName[data.response.docs[0]["fullName_s"]] = resultRow;
       }
-      if(data.response.docs[0].fileMain_s!=undefined){
-         info.fileMain = "<a href=\""+data.response.docs[0].fileMain_s+"\">"+data.response.docs[0].fileMain_s+"</a>";
-      }
-      if(info.year==undefined){
-         info.year = data.response.docs[0].producedDateY_i;
-      }
+      result[lastSentQuery+1] = resultRow;
    }
-   
-   // Prepare values for the next table row to be added
-   var col3 = "non";
-   if(info.is_oa){col3 = "oui";}
-
-   var col4 = "non";
-   if(info.journal_is_oa){col4 = "oui";}
-
-   var col5 = "non";
-   if(info.journal_is_in_doaj){col5 = "oui";}
-
-   var col2 = "non-OA";
-   if((col3 == "oui")||(col4 == "oui")||(col5 == "oui")||(info.best_oa_location != undefined)||((info.linkExtId != undefined)&&(info.linkExtId != "istex"))||(info.fileMain != undefined)){
-      col2 = "OA";
-      // increase the number of open access publications found
-      oa += 1;
-   }
-   
-   // Add a new row to the table
-   $("#results").find("tbody").append("\n<tr>"
-   +"<td><small><a href=\"http://dx.doi.org/"+author[lastSentQuery]+"\">"+author[lastSentQuery]+"</a></small></td>"
-   +"<td>"+col2+"</td>"
-   +"<td>"+col3+"</td>"
-   +"<td>"+col4+"</td>"
-   +"<td>"+col5+"</td>"
-   +"<td>"+info.best_oa_location+"</td>"
-   +"<td>"+info.halId+"</td>"
-   +"<td>"+info.halNb+"</td>"
-   +"<td>"+info.linkExtId+"</td>"
-   +"<td><small>"+info.linkExtUrl+"</small></td>"
-   +"<td><small>"+info.fileMain+"</small></td>"
-   +"<td>"+info.year+"</td>"
-   +"</tr>");
-   
-   */
    
    // Go the next author query
    lastReceivedQuery += 1;
    
    // Update the progress rate and the open access rate
-   $("#progress").html(parseInt(1000.0*(lastReceivedQuery+1)/author.length)/10);
+   $("#progress").html(parseInt(1000.0*(lastReceivedQuery+1)/Object.keys(author).length)/10);
    
 }
+
+
+// Create a string containing the current date of the form YYYY-MM-DD
+function buildDate(){
+   // Build date
+   var m = new Date(Date.now()).getMonth()+1;
+   if(m<10){
+      m="0"+m;
+   }
+   var d = new Date(Date.now()).getDate();
+   if(d<10){
+      d="0"+d;
+   }
+   return new Date(Date.now()).getFullYear()+"-"+m+"-"+d+"_"+new Date(Date.now()).getHours()+"h"+new Date(Date.now()).getMinutes();
+}
+
+
 
 /************************************/
 /* code below used to download the CSV file
 /* derived from the code by A.H. Bitubekk
 /* from https://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
 /************************************/
+
 
 // Download the table containing the results
 $("#download").on("click",function(e){
@@ -201,20 +240,38 @@ $("#download").on("click",function(e){
       csvContent += index < result.length ? dString + '\n' : dString;
    });
    
-   // Build date
-   var m = new Date(Date.now()).getMonth()+1;
-   if(m<10){
-      m="0"+m;
-   }
-   var d = new Date(Date.now()).getDate();
-   if(d<10){
-      d="0"+d;
-   }
-   var date = new Date(Date.now()).getFullYear()+"-"+m+"-"+d+"_"+new Date(Date.now()).getHours()+"h"+new Date(Date.now()).getMinutes();
+   // Save file
+   download(csvContent, 'download-'+buildDate()+'.csv', 'text/csv;encoding:utf-8');
+})
+
+
+// Download the table containing the results without duplicates
+$("#download2").on("click",function(e){
+
+   // CSV file
+   var csvContent = '';
+
+   e.preventDefault();
+   
+   // Building the CSV from the Object associating an array to each key
+   // Each column is separated by ";" and new line "\n" for next row
+   Object.keys(resultByName).forEach(function(infoArray,index) {
+      var dString = "";
+      resultByName[infoArray].forEach(function(str){
+         if(str==undefined){
+            str="";
+         } else {
+            str=""+str;
+         }
+         dString += '"'+str.replace(/"/g,'""')+'";';
+      })
+      csvContent += index < result.length ? dString + '\n' : dString;
+   });
    
    // Save file
-   download(csvContent, 'download-'+date+'.csv', 'text/csv;encoding:utf-8');
+   download(csvContent, 'download-noDuplicate-'+buildDate()+'.csv', 'text/csv;encoding:utf-8');
 })
+
 
 // The download function takes a CSV string, the filename and mimeType as parameters
 // Scroll/look down at the bottom of this snippet to see how download is called
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ExtracteurUnpaywall
 Évaluer la proportion des publications en libre accès parmi une liste de DOI
+
+## Autres fonctionnalités
+* recenser les informations sur les auteurs affiliés à une structure : ExtracteurAuteur.html
